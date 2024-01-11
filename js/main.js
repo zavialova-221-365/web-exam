d5237264bc172773affd1401885c439918cf79db
zavialova-221-365 <zavdasha2004@mail.ru>
2024-01-11 11:55:56 +0300

    добавлена функция заполнения таблицы гидов

--- js/main.js
+++ js/main.js
@@ -155,5 +155,67 @@ document.addEventListener('DOMContentLoaded', function () {
             pagination.appendChild(li);
         }
     }
+    let selectedRoute = '';
+    function loadGuidesForRoute(routeId, routeName) {
+        const apiUrl = `http://exam-2023-1-api.std-900.ist.mospolytech.ru/api/routes/${routeId}/guides?api_key=3990d76f-4908-438c-a2e9-90a0a642eb96`;
+        selectedRoute = routeName;
+        const textforguide = document.getElementById("textforguide");
+        const guideRoute = routeName;
 
+
+
+        // Обновляем текст в элементе "textforguide"
+        textforguide.textContent = `Доступные гиды по маршруту ${guideRoute}`;
+        fetch(apiUrl)
+            .then(response => response.json())
+            .then(data => {
+                guidesTableBody.innerHTML = '';
+
+                data.forEach(guide => {
+                    const row = document.createElement('tr');
+                    const nameCell = document.createElement('td');
+                    const languageCell = document.createElement('td');
+                    const workExperienceCell = document.createElement('td');
+                    const pricePerHourCell = document.createElement('td');
+                    const selectButtonCell = document.createElement('td');
+                    const selectButton = document.createElement('button');
+
+                    nameCell.textContent = guide.name;
+                    languageCell.textContent = guide.language;
+                    workExperienceCell.textContent = guide.workExperience;
+                    pricePerHourCell.textContent = guide.pricePerHour;
+
+                    selectButton.textContent = 'Выбрать';
+                    selectButton.classList.add('btn', 'databutton', 'btn-secondary');
+                    selectButton.setAttribute('data-price-per-hour', guide.pricePerHour);
+                    selectButton.setAttribute('data-guide-id', guide.id);
+                    selectButton.setAttribute('data-route-id', routeId);
+                    selectButton.addEventListener('click', function () {
+
+                        selectButton.classList.toggle('selectedG');
+
+                        const guideName = guide.name;
+
+                        document.getElementById('selectedGuide').value = guideName;
+                        document.getElementById('selectedRoute').value = selectedRoute;
+
+                        const orderModal = new bootstrap.Modal(document.getElementById('orderModal'));
+                        orderModal.show();
+                    });
+
+                    selectButtonCell.appendChild(selectButton);
+
+                    row.appendChild(nameCell);
+                    row.appendChild(languageCell);
+                    row.appendChild(workExperienceCell);
+                    row.appendChild(pricePerHourCell);
+                    row.appendChild(selectButtonCell);
+
+                    guidesTableBody.appendChild(row);
+                });
+            })
+            .catch(error => {
+                console.error('Ошибка получения данных о гидах:', error);
+            });
+    }
 });
